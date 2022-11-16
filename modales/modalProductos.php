a5e41ae727ef534cf4c92a7870a0564a25c4ec92
wilmer <wilmeruribe777@gmail.com>
2022-11-16 16:00:56 -0400

    Creando ventanas modal-productos

--- modales/modalProductos.php
+++ modales/modalProductos.php
@@ -3,8 +3,15 @@
 		<div class="modal-content">
 			<div class="modal-header">
 				<h1 class="modal-title fs-5" id="nuevoModalLabel">Titulo Modal</h1>
+				<button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
+			</div>
+			<div class="modal-body">
+				...
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+				<button type="button" class="btn btn-primary"> Guardar cambios</button>
 			</div>
 		</div>
-		
 	</div>
 </div>
