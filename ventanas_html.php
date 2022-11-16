1ad93ed4eae40bdbd40bcac34459018c5c2c54b8
wilmer <wilmeruribe777@gmail.com>
2022-11-16 17:06:51 -0400

    Creando ventanas modal-productos

--- ventanas_html.php
+++ ventanas_html.php
@@ -14,14 +14,7 @@
 	}
 ?>
 
-<!-- MESSAGES -->
-<?php if (isset($_SESSION['message'])) { ?>
-	<div class="alert alert-<?= $_SESSION['message_type']?> alert-dismissible fade show" role="alert">
-		<?= $_SESSION['message']?>
-		<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
-		</button>
-	</div>
-<?php unset($_SESSION['message']); 	 } ?>
+
  <!-- Maestro Productos -->
 <div class="main container p-4  <?php if ($Tip_form_maestro<>'producto' ) { echo $ocultar ; } ?>">
 	<div class="row">
