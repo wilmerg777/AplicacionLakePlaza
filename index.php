ecfccb50fc1297d780fd2ed2e633095cafdb15b6
wilmer <wilmeruribe777@gmail.com>
2022-11-16 17:06:07 -0400

    Creando ventanas modal-productos

--- index.php
+++ index.php
@@ -4,6 +4,14 @@
 	include("includes/header.php");
 	include("db.php");
 ?>
+<!-- MESSAGES -->
+<?php if (isset($_SESSION['message'])) { ?>
+	<div class="alert alert-<?= $_SESSION['message_type']?> alert-dismissible fade show" role="alert">
+		<?= $_SESSION['message']?>
+		<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+		</button>
+	</div>
+<?php unset($_SESSION['message']); 	 } ?>
 
 <div class="container p-4 ">
 		<?php include('list_contratos.php') ?>
