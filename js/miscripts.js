6d090a8672302de4a4cd03d26f27f86a59198d26
wilmer <wilmeruribe777@gmail.com>
2022-11-21 23:57:34 -0400

    Cargando inputs de form contratos con ajax

--- js/miscripts.js
+++ js/miscripts.js
@@ -1,14 +1,12 @@
 document.getElementById('ced_titular1').addEventListener("blur", getCedula)
 
-document.getElementById('form_contratos')
+//document.getElementById('form_contratos')
 
 
 function getCedula(){
 
 	let cedula1 = document.getElementById('ced_titular1').value
 	let lista = document.getElementById('lista')
-
-console.log(lista)
 
 	let cod_php = "verificar_cedula.php"
 	let formData = new FormData()
@@ -20,10 +18,10 @@ console.log(lista)
 		body: formData,
 		mode: "cors"
 	}).then(response => response.json())
-	.then(data => {
-		console.log(data)
-		lista.style.display = 'block'
-		lista.innerHTML = data
+	.then(data=> {
+		document.getElementById('nom_titular1').value=(data[0].nombre_afil_natu)
+		document.getElementById('ape_titular1').value=(data[0].apellido_afil_natu)
+
 	})
 	.catch(err => console.log(err))
 
--- verificar_cedula.php
+++ verificar_cedula.php
@@ -8,13 +8,12 @@
 		 afiliados_natu where cod_afil_natu like ? order by 1";
 
 		$resultado = $conn->prepare($sql);
-		$resultado->execute([$cedula. "%"]);
+		$resultado->execute([$cedula]);
 
 		$html = "";
 
-		while ($row = $resultado->fetch(PDO::FETCH_ASSOC)){
-		    $html .= $row['cod_afil_natu']." , ".$row['nombre_afil_natu'];
-		}
-		echo json_encode($html, JSON_UNESCAPED_UNICODE);
+		$row = $resultado->fetchall(PDO::FETCH_ASSOC);
+
+		printf(json_encode($row));
 
  ?>
