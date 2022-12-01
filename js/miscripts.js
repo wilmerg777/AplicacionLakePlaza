581480b4a2863a9bacc979e64ed77ca5b46d3715
wilmer <wilmeruribe777@gmail.com>
2022-12-01 17:02:32 -0400

    revisando fetch javascript

--- js/miscripts.js
+++ js/miscripts.js
@@ -1,5 +1,6 @@
 let d = document;
-console.log('entrando');
+let now = new  Date().toLocaleDateString('es-ve', { weekday:"long", year:"numeric", month:"short", day:"numeric"});
+console.log('entrando: '+now);
 // console.log(d);
 
 d.addEventListener('focusout',(e)=>{
@@ -7,6 +8,9 @@ d.addEventListener('focusout',(e)=>{
 	if (elemento.id=='ced_titular1')
 		getCedula(elemento.value)
 		//console.log(elemento.value + " elemento disparador");
+
+	if (elemento.id=='contrato')
+		retorna_contrato(elemento.value)
 },false);
 
 d.addEventListener('click',(e)=>{
@@ -21,14 +25,16 @@ d.addEventListener('click',(e)=>{
 
 //document.getElementById('form_contratos')
 
-function retorna_contrato(){
-	let contrato = document.getElementById('contrato')
+function retorna_contrato(valor){
 
-	if (contrato.value.length>7 || contrato.value.length<6 || contrato.value.length==0) {
+	let nuevo_contrato = valor ;
+	console.log(nuevo_contrato.length) ;
+
+	if (nuevo_contrato.length>7 || nuevo_contrato.length<6 || nuevo_contrato.length==0) {
 		
 		document.getElementById('contrato').classList.add('bg-warning')
 		document.getElementById('contrato').value="";
-		document.getElementById('contrato').focus();
+		document.getElementById('contrato').focus;
 	}else {
 		document.getElementById('contrato').classList.remove('bg-warning')
 		document.getElementById('contrato').classList.add('bg-primary'  , 'text-white')
@@ -37,6 +43,10 @@ function retorna_contrato(){
 
 
 function getCedula(cedula){
+
+	let prueba = fetch ('index.php')
+
+	console.log(prueba);
 
 	//let cedula1 = document.getElementById('ced_titular1').value
 	let ced = cedula
@@ -53,6 +63,7 @@ function getCedula(cedula){
 		mode: "cors"
 	}).then(response => response.json())
 	.then(data=> {
+		alert(data);
 		document.getElementById('nom_titular1').value=(data[0].nombre_afil_natu)
 		document.getElementById('ape_titular1').value=(data[0].apellido_afil_natu)
 
@@ -60,5 +71,3 @@ function getCedula(cedula){
 	.catch(err => console.log(err))
 
 }
-
-
